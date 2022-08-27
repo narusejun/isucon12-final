8ab9ee19b0a27cff2e6da2363f19e7729fcc539b
wtks <30363887+wtks@users.noreply.github.com>
2022-08-27 09:09:25 +0900

    common

--- s1/deploy.sh
+++ s1/deploy.sh
@@ -15,15 +15,3 @@ sudo cp -f env.sh /home/isucon/env.sh
 #sudo systemctl restart mariadb
 #sudo systemctl reload nginx
 #sudo systemctl restart isuxxxx
-
-# slow query logの有効化
-QUERY="
-set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
-set global long_query_time = 0;
-set global slow_query_log = ON;
-"
-echo $QUERY | sudo mysql -uroot
-
-# log permission
-sudo chmod -R 777 /var/log/nginx
-sudo chmod -R 777 /var/log/mysql
